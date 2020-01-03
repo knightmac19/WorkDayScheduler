85263ebe5e720df7c4d1f3613a9e4a1e98ff7722
Patrick Dunn <pmdunn78@gmail.com>
2020-01-03 12:44:25 -0500

    displaying current time on a 1-second interval

--- script.js
+++ script.js
@@ -1,54 +1,74 @@
-var hoursArray = [
-    "9AM",
-    "10AM",
-    "11AM",
-    "12PM",
-    "1PM",
-    "2PM",
-    "3PM",
-    "4PM",
-    "5PM"
-];
+$(document).ready(function() {
+    var secondsLeft = 1;
 
-//function that iterates through the hoursArray and creates a row with 3 columns for each index
-var populateTable = function() {
-    for (i = 0; i < hoursArray.length; i++) {
-        var tableRow = $("<tr>");
-
-        var saveBtn = $("<button>").attr("class", "btn btn-primary save");
-        saveBtn.text("Save");
-
-        var toDos = $("<input>").css("width", "100%");
-        toDos.css("border", "3px dotted blue");
-        toDos.attr("data-form", hoursArray[i]);
-        
-        var hourColumn = $("<td>").text(hoursArray[i]);
-        var toDoColumn = $("<td>").html(toDos);
-        var saveColumn = $("<td>").html(saveBtn);
-        
-        tableRow.append(hourColumn, toDoColumn, saveColumn);
-        $("tbody").append(tableRow);
-    }
-}
-
-//use jQuery .prev() to grab previous sibling of save button
-//give all save buttons a common class
-function saveToLocalStorage() {
-    var userInput = $(".save").prev().val();
+    //array of hours
+    var hoursArray = [
+        "9AM",
+        "10AM",
+        "11AM",
+        "12PM",
+        "1PM",
+        "2PM",
+        "3PM",
+        "4PM",
+        "5PM"
+    ];
     
-    //currently logs "undefined"
-    console.log(userInput);
-}
+    //iterating through the hoursArray and creating a 3-column row for each index
+    var populateTable = function() {
+        for (i = 0; i < hoursArray.length; i++) {
+            var tableRow = $("<tr>");
+            tableRow.attr("data-form", hoursArray[i]);
+    
+            var saveBtn = $("<button>").attr("class", "btn btn-primary");
+            saveBtn.text("Save");
+    
+            var toDos = $("<input>");
+            toDos.attr("data-form", hoursArray[i]);
+            
+            var hourColumn = $("<td>").text(hoursArray[i]);
+            var toDoColumn = $("<td>").html(toDos);
+            var saveColumn = $("<td>").html(saveBtn);
+            saveColumn.attr("class", "save");
+            
+            tableRow.append(hourColumn, toDoColumn, saveColumn);
+            $("tbody").append(tableRow);
+        }
+    }
+    
+    //saving UI to local storage
+    function saveToLocalStorage(event) {
+        event.preventDefault();
+        var userInput = $(this).prev().children();
+        var userInputValue = userInput.val();
+        console.log(userInputValue);
+        console.log("save button working");
+        localStorage.setItem("userInputValue", userInputValue);
+        
+        var retrievedInput = localStorage.getItem('userInputValue');
+        $(userInput).text(retrievedInput);
+        console.log("retrieved from storage: " + retrievedInput);
 
-//set momentTime within a function that runs on a 1-second interval
-var momentTime = moment().format("LTS");
-var currentTime = $("#moment").text(momentTime);
+    }
+    
+    //displaying current time on a 1-second interval
+    function displayTime() {
+        var timerInterval = setInterval(function() {
+            var currentTime = moment().format("LTS");
+            $("#current").text(currentTime);
+            secondsLeft--;
+        
+            if (secondsLeft === 0) {
+              clearInterval(timerInterval);
+              displayTime();
+            }
+          }, 1000);        
+    }
+    
+    //conditionally formatting the rows based on current time
 
-populateTable();
-$(document).on("click", ".save", saveToLocalStorage);
-
-
-
-
-
-
+    
+    populateTable();
+    displayTime();
+    $(document).on("click", ".save", saveToLocalStorage);
+});
